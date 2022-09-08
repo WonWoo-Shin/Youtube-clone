444a9482003728509a4dfdacc7978b49ae03d948
wonwoo <93998008+WonWoo-Shin@users.noreply.github.com>
2022-09-08 15:04:42 +0900

    add pug

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,3 +1,42 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const watch = (req, res) => res.send("Watch");
-export const edit = (req, res) => res.send("Edit");
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createAt: "2 minutes ago",
+    views: 1,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
+
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", videos });
+export const watch = (req, res) => {
+  const video = videos[req.params.id - 1];
+  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
+};
+export const getEdit = (req, res) => {
+  const video = videos[req.params.id - 1];
+  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+};
+export const postEdit = (req, res) => {
+  const video = videos[req.params.id - 1];
+  video.title = req.body.title;
+  return res.redirect(`/video/${req.params.id}`);
+};
--- src/router/videoRouter.js
+++ src/router/videoRouter.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { watch, edit } from "../controller/videoController";
+import { watch, getEdit, postEdit } from "../controller/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/watch", watch);
-videoRouter.get("/edit", edit);
+videoRouter.get("/:id(\\d+)", watch);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 
 export default videoRouter;
